ef576f746e0f428603eca437da2622827063557e
itt-n <itt_nakashima@worksid.co.jp>
2026-05-19 14:03:41 +0900

    戦闘設定の自動検索機能追加

--- js/prevent_arcade_move.js
+++ js/prevent_arcade_move.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         アーケード表示簡易化
+// @name         アーケード簡易化
 // @namespace    http://tampermonkey.net/
 // @version      2026-05-15
-// @description  アーケードモードで、戦闘ログ画面への遷移をキャンセルする
+// @description  アーケードモード攻略時に発生するいろんな不満の解消
 // @author       なしなし
 // @match        https://wdrb.work/bo5/battle_lobby.php?mode=arcade
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wdrb.work/bo5
@@ -16,32 +16,48 @@
 		f(e);
 		return e;
 	}
-
-	const key = 'prevent_arcade_move';
-
-	const options = document.querySelector('.setup_link');
-	options.parentElement.insertBefore(bake('label', e => {
-		e.appendChild(bake('input', e => {
-			e.type = 'checkbox';
-			e.checked = localStorage.getItem(key) === 'true';
-			e.addEventListener('change', () => localStorage.setItem(key, e.checked ? 'true' : 'false'));
-		}));
-		e.appendChild(document.createTextNode('戦闘結果の表示をスキップ'));
-	}), options);
-
 	const form = document.getElementById('btlb_form_arcade');
-	form.addEventListener('submit', e => {
-		if (localStorage.getItem(key) !== 'true') return;
-		e.preventDefault();
-		fetch(form.action, { method: form.method, body: new FormData(form) }).then(r => {
-			if (r.ok) {
-				location.reload();
-			} else {
-				console.error('サーバーエラーが発生しました。');
-				alert('サーバーエラーが発生しました。');
-			}
-		}).catch(e => {
-			console.error('通信エラー:', e);
+
+	// ページ遷移省略
+	{
+		const key = 'prevent_arcade_move';
+
+		const options = document.querySelector('.setup_link');
+		options.parentElement.insertBefore(bake('label', e => {
+			e.appendChild(bake('input', e => {
+				e.type = 'checkbox';
+				e.checked = localStorage.getItem(key) === 'true';
+				e.addEventListener('change', () => localStorage.setItem(key, e.checked ? 'true' : 'false'));
+			}));
+			e.appendChild(document.createTextNode('戦闘結果の表示をスキップ'));
+		}), options);
+
+		form.addEventListener('submit', e => {
+			if (localStorage.getItem(key) !== 'true') return;
+			e.preventDefault();
+			fetch(form.action, { method: form.method, body: new FormData(form) }).then(r => {
+				if (r.ok) {
+					location.reload();
+				} else {
+					console.error('サーバーエラーが発生しました。');
+					alert('サーバーエラーが発生しました。');
+				}
+			}).catch(e => {
+				console.error('通信エラー:', e);
+			});
 		});
-	});
+	}
+
+	// 選択時名前で自動検索
+	{
+		const search_box = document.getElementById('drilldown');
+		for (const e of form.querySelector('.battle_npc').children) {
+			e.addEventListener('click', () => {
+				const stage = e.dataset.stage;
+				const name = form.querySelector(`.next_ch[data-npc="${stage}"] [data-name]`).dataset.name;
+				search_box.value = `${name}|${stage}`;
+				search_box.dispatchEvent(new Event('input'));
+			})
+		}
+	}
 })();
